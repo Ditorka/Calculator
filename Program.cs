4f15490cf8c3c7a006b77795a2c78eb96af1651b
Dimira <73307507+Ditorka@users.noreply.github.com>
2022-10-12 14:17:25 +0300

    Коментар: добавлено опис функції

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ namespace BalanceCalculator
         [STAThread]
         static void Main()
         {
+            //Ініціалізація графічної форми
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
